abd2026ff9dbed26b0902a3c198741788f4f3a53
Hassanelashram <elashramhassan@gmail.com>
2020-06-08 11:21:52 +0200

    done

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,22 @@
+class PagesController < ApplicationController
+  def index
+    if params[:question]
+      redirect_to answer_path
+    end
+  end
+
+  def answer
+    @question = params[:question]
+    @answer = coach_answer(@question)
+  end
+
+  def coach_answer(your_message)
+    if your_message.include?("?")
+      return "Silly question, get dressed and go to work!"
+    elsif your_message == "I am going to work right now!"
+      return ""
+    else
+      return "I don't care, get dressed and go to work!"
+    end
+  end
+end
